937c204a77be868a6edcd9509a464472fa3e54ed
Kent Delante <kent@kentdelante.com>
2024-09-03 07:30:15 +0800

    Add poppins font

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+				poppins: ['Poppins', ...defaultTheme.fontFamily.sans]
 			},
 			colors: {
 				seraphique: '#3A5033'
