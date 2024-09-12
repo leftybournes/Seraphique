594cfc07f20d43e5998519a2bcd4156e92111698
Kent Delante <kent@kentdelante.com>
2024-09-12 16:46:06 +0800

    Increase width of elements and their images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     has_one_attached :image do |attachable|
-        attachable.variant :thumb, resize_to_limit: [230, 230], preprocessed: true
+        attachable.variant :thumb, resize_to_limit: [260, 260], preprocessed: true
     end
 
     belongs_to :category
