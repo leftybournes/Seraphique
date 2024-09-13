e3646c4fffcb783646706c9829a34ae7e9b2fd16
Kent Delante <kent@kentdelante.com>
2024-09-13 15:37:14 +0800

    Adjust product card size and spacing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     has_one_attached :image do |attachable|
-        attachable.variant :thumb, resize_to_limit: [260, 260], preprocessed: true
+        attachable.variant :thumb, resize_to_limit: [300, 300], preprocessed: true
     end
 
     belongs_to :category
