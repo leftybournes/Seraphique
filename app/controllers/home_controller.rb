0a9d8db9b7398ee5045a888202ae77e50f2b7447
Kent Delante <kent@kentdelante.com>
2024-09-19 15:40:09 +0800

    Add product list to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 class HomeController < ApplicationController
-  layout "homepage"
+    layout "homepage"
 
-  def index
-  end
+    def index
+        @products =
+            if params[:category].present?
+                Product.where(category_id: params[:category])
+            else
+                Product.all
+            end
+
+        @categories = Category.all
+        @selected_category =
+            if params[:category].present?
+                params[:category].to_i
+            else
+                nil
+            end
+    end
 end
