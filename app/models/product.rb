17ea90fd49c353bcf2bdc9f4c3970bc504cc29ad
Kent Delante <kent@kentdelante.com>
2024-09-25 02:52:13 +0800

    Implement shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
     has_one_attached :image do |attachable|
         attachable.variant :thumb, resize_to_limit: [300, 300], preprocessed: true
+        attachable.variant :small, resize_to_limit: [100, 100], preprocessed: true
     end
 
     has_many :reviews
