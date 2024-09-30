b044440976dd8a75f3f82204e05d7ec08e4cd745
Kent Delante <kent@kentdelante.com>
2024-09-30 14:01:48 +0800

    Change implementation of user seed

--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -1,12 +1,16 @@
 require "faker"
 
+users = []
+
 rand(1000..1500).times do
     date = Faker::Date.between(from: 5.years.ago, to: Date.today)
-    User.create(
-        full_name: Faker::Name.unique.name,
-        email: Faker::Internet.unique.email,
-        password: "password",
-        created_at: date,
-        updated_at: date
+    users.push(
+        {
+            full_name: Faker::Name.unique.name,
+            email: Faker::Internet.unique.email,
+            password: "password"
+        }
     )
 end
+
+User.create(users)
