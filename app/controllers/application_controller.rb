0036683695dc2b7194e59f4f7e8ee4db2d819260
Kent Delante <kent@kentdelante.com>
2024-10-07 11:48:56 +0800

    Change how header variables are initialized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :initialize_header_data
+
     attr_reader :categories
+    attr_reader :shopping_cart_item_count
 
-    def initialize
-        super
-
+    def initialize_header_data
         @categories = Category.all
+        @shopping_cart_item_count = 0
+
+        if user_signed_in?
+            @shopping_cart_item_count = current_user.shopping_cart_items.sum(:quantity)
+        end
     end
 end
