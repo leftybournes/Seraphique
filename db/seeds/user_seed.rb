16fcdd42e784a4f43435d68de38a0bc7774c0786
Kent Delante <kent@kentdelante.com>
2024-10-14 17:30:33 +0800

    Add address seeder

--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -14,3 +14,16 @@ rand(1000..1500).times do
 end
 
 User.create(users)
+
+User.all.each do |user|
+    user_address = {
+        line_1: Faker::Address.street_address,
+        line_2: Faker::Address.secondary_address,
+        city: Faker::Address.city,
+        state: Faker::Address.state,
+        country: Faker::Address.country,
+        postal_code: Faker::Address.postcode
+    }
+
+    user.addresses.create(user_address)
+end
