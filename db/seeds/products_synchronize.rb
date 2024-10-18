d8b15fcfa9b9fa349659d31658b8b71de1b59192
Kent Delante <kent@kentdelante.com>
2024-10-18 19:43:23 +0800

    Implement syncing with stripe if data is available from there

--- db/seeds/products_synchronize.rb
+++ db/seeds/products_synchronize.rb
@@ -27,6 +27,48 @@ def products_synchronize
     }
   ]
 
-  Stripe::Product.list.data.each do |stripe_product|
+  Stripe::Product.list(limit: 100).data.each do |stripe_product|
+    price = Stripe::Price.retrieve(stripe_product.default_price)
+
+    params = {
+      name: stripe_product.name,
+      summary: stripe_product.metadata.summary,
+      quantity: stripe_product.metadata.quantity.to_i,
+      price: price.unit_amount / 100,
+      created_at: stripe_product.metadata.created_at,
+      updated_at: stripe_product.metadata.created_at
+    }
+
+
+    product = Product.create(params)
+
+    category = Category.find_by(name: stripe_product.metadata.categories)
+    product.categories << category
+    product.variants.create(product_variants)
+    product.usage_directions.create(product_usage_directions)
+
+    is_best_seller = [true, false].sample
+    is_new_product = [true, false].sample
+
+    if is_best_seller
+      best_seller_category = Category.find_by(name: "Best Seller")
+      product.categories << best_seller_category
+    end
+
+    if is_new_product
+      new_category = Category.find_by(name: "New Product")
+      product.categories << new_category
+    end
+
+    image_name = product.name
+                   .gsub(" ", "_")
+                   .downcase + ".png"
+
+    product.image.attach(
+      io: File.open(Rails.root.join("app/assets/images/products/bottle.png")),
+      filename: image_name
+    )
+
+    product.stripe_product = StripeProduct.new(stripe_id: stripe_product.id)
   end
 end
