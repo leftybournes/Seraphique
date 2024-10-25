e4408acdd75f459e246e1d273bd8722f865edeb1
Kent Delante <kent@kentdelante.com>
2024-10-25 16:33:33 +0800

    Create user address directly

--- db/seeds/user_seed.rb
+++ db/seeds/user_seed.rb
@@ -16,14 +16,15 @@ end
 User.create(users)
 
 User.all.each do |user|
-    user_address = {
-        line_1: Faker::Address.street_address,
-        line_2: Faker::Address.secondary_address,
-        city: Faker::Address.city,
-        state: Faker::Address.state,
-        country: Faker::Address.country,
-        postal_code: Faker::Address.postcode
+  user.addresses.create(
+    {
+      user_id: user.id,
+      line_1: Faker::Address.street_address,
+      line_2: Faker::Address.secondary_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state,
+      country: Faker::Address.country,
+      postal_code: Faker::Address.postcode
     }
-
-    user.addresses.create(user_address)
+  )
 end
